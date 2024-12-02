161398687034ae5d32e65eb5157027b4e5f3719d
Cameron <cameron.wragg@hotmail.co.uk>
2024-12-02 15:32:54 +0000

    Adding D1P1 solution

--- src/day_1.py
+++ src/day_1.py
@@ -1,2 +1,17 @@
+from re import split
+
+from common.input import read_to_string
+
+
 def part_1():
-    pass
+    input = split(r"   |\n", read_to_string("src/data/day_1"))
+    return sum(
+        [
+            abs(int(r_element) - int(l_element))
+            for l_element, r_element in zip(sorted(input[::2]), sorted(input[1::2]), strict=True)
+        ]
+    )
+
+
+if __name__ == "__main__":
+    print(f"Day 1 Part 1: {part_1()}")
