2706d66fa4f26dee890eda2f6f754b6a8fa5cc3a
Cameron <cameron.wragg@hotmail.co.uk>
2024-12-02 16:53:08 +0000

    Adding day 2 data and read_to_list function

--- src/common/input.py
+++ src/common/input.py
@@ -1,2 +1,11 @@
 def read_to_string(file_location: str) -> str:
     return open(file_location).read().rstrip("\n")
+
+
+def read_to_list(file_location: str, to_int: bool, separator: str) -> list[str] | list[int]:
+    _list = read_to_string(file_location).split(separator)
+
+    if to_int:
+        return [int(element.rstrip("\n")) for element in _list]
+
+    return _list
