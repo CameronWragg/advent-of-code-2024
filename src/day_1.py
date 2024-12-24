526b5f822eb4783789d1371d5e22cc447f77c3eb
Cameron <cameron.wragg@hotmail.co.uk>
2024-12-24 12:08:47 +0000

    Updating python version, poetry config, ruff rules

--- src/day_1.py
+++ src/day_1.py
@@ -1,23 +1,23 @@
 from collections import Counter
 from re import split
 
-from common.input import read_to_string
+from common.inputs import read_to_string
 
 
-def part_1(input: list[int]) -> int:
+def part_1(data: list[int]) -> int:
     return sum(
         [
             abs(r_element - l_element)
-            for l_element, r_element in zip(sorted(input[::2]), sorted(input[1::2]), strict=True)
-        ]
+            for l_element, r_element in zip(sorted(data[::2]), sorted(data[1::2]), strict=True)
+        ],
     )
 
 
-def part_2(input: list[int]) -> int:
-    r_counter = Counter(input[1::2])
-    return sum([l_element * r_counter[l_element] for l_element in input[::2]])
+def part_2(data: list[int]) -> int:
+    r_counter = Counter(data[1::2])
+    return sum([l_element * r_counter[l_element] for l_element in data[::2]])
 
 
 if __name__ == "__main__":
-    input = list(map(int, split(r"   |\n", read_to_string("src/data/day_1"))))
-    print(f"Day 1 Part 1: {part_1(input)}\nDay 1 Part 2: {part_2(input)}")
+    data = list(map(int, split(r"   |\n", read_to_string("src/data/day_1"))))
+    print(f"Day 1 Part 1: {part_1(data)}\nDay 1 Part 2: {part_2(data)}")
